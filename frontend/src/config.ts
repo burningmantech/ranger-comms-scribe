7ac2e71c29a0754dde7aa43e1de5cb7abe73d0e2
Alex Young <alexander.young@gmail.com>
2025-06-02 12:55:44 -0700

    Added Google Client ID

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
-export const GOOGLE_CLIENT_ID = ''; // TODO: Add one
+export const GOOGLE_CLIENT_ID = '402914910938-47o6ff5rkig658lr4k51rmrmlbm4s4qg.apps.googleusercontent.com';
 
 // Use environment variable for API_URL if available (for local development)
 export const API_URL = process.env.REACT_APP_API_URL || 'https://api.scrivenly.com';
