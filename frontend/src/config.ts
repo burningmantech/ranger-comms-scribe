268eb20df0034dbc27662a7d68acb402044b385a
Alex Young <alexander.young@gmail.com>
2025-06-02 10:48:14 -0700

    More conversion things

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
-export const GOOGLE_CLIENT_ID = '506064022717-4f6o2i9v8560g5g3ns45h1al3sr2p8j0.apps.googleusercontent.com';
+export const GOOGLE_CLIENT_ID = ''; // TODO: Add one
 
 // Use environment variable for API_URL if available (for local development)
 export const API_URL = process.env.REACT_APP_API_URL || 'https://api.scrivenly.com';
